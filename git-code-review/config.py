1d05f0bb9d4ec9d759e061ce8f1e884ae106d35f
AJ <naqibprio10@gmail.com>
2024-08-31 20:07:10 -0500

    chore

--- git-code-review/config.py
+++ git-code-review/config.py
@@ -1,12 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env
+load_dotenv()
+
 # Configuration and constants
-
-# TODO: Move sensitive information to environment variables
-
 class Config:
     def __init__(self):
-        self.GITHUB_ACCESS_TOKEN = "REMOVED_GITHUB_TOKEN"
-        self.OPENAI_API_KEY = "REMOVED_OPENAI_API_KEY"
-        self.TEST_MODE = False
+        self.GITHUB_ACCESS_TOKEN = os.getenv("GITHUB_ACCESS_TOKEN")
+        self.OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+        self.TEST_MODE = os.getenv("TEST_MODE", "False") == "True"
 
 # Singleton instance
 config = Config()
